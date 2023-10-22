e941d7dc997cd5875c63ba27711d136468bf6e6b
Paris Tsiogas <ptsiogas@gmail.com>
2023-10-22 16:01:47 +0300

    build: set target sdk to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.ptsiogas.composestories.sample"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.ptsiogas.composestories.sample"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
